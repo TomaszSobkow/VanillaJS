bb11156ef396e4a70d21238aa5eaaf80e3c9865c
TomaszSobkow <sobeczki@gmail.com>
2025-05-09 14:59:03 +0100

    added to on project

--- hex/hex.js
+++ hex/hex.js
@@ -3,12 +3,12 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function(){
-   let hexColor = '#';
-   for(let i = 0; i<6; i++){
+    let hexColor = '#';
+    for(let i = 0; i<6; i++){
     hexColor += hex[getRandomNumber()]	
-   } 
-   color.textContent = hexColor;
-   document.body.style.backgroundColor = hexColor;
+    } 
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
 });
 
 function getRandomNumber(){
